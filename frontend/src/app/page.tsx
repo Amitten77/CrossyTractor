c55f0d9bce18ce22390f6672ff8257fc48e7dbea
Ameat77 <amit.krishnaiyer@gmail.com>
2023-07-19 11:12:40 -0500

    made home page and leaderboard look nicer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,26 @@
-import Image from 'next/image'
+'use client'
+import { useState } from 'react';
 
 export default function Home() {
+  const [name, setName] = useState<string>("Player")
+
+  const handleOnClick = () => {
+    localStorage.setItem('name', name);
+  }
+
+  const handleInputChange = (e: any) => {
+    setName(e.target.value);
+  }
+
+
   return (
     <>
       <h1 className="text-6xl flex justify-center mt-6">Crossy Tractor!!</h1>
+      <h2>What is your name?</h2>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Your Name" onChange={handleInputChange}/>
       <div id="control-panel" className='flex justify-center mt-8'>
         <a href="game">
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-6'>Play Game</button>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded mr-6' onClick={handleOnClick}>Play Game</button>
         </a>
         <a href="leaderboard">
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'>Check out Leaderboard</button>
